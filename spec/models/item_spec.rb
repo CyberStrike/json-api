4251d87056dce517c035e772535f62f3def949f4
Chris Scott <cyberstrike@gmail.com>
2017-11-04 03:44:19 -0400

    added item tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it { is_expected.to belong_to(:todo) }
+  end
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
