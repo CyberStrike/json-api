9294ecde43144ee11599eebf0e971e8275b6f294
Chris Scott <cyberstrike@gmail.com>
2017-11-04 03:31:56 -0400

    passing todo spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  it { is_expected.to have_many(:items) }
-  it { is_expected.to validate_presence_of :title }
-  it { is_expected.to validate_presence_of :created_by}
-  it { should have_many(:items).dependent(:destroy) }
+  context 'Associations' do
+    it 'has many items' do
+      is_expected.to have_many(:items)
+    end
+
+    it { should have_many(:items).dependent(:destroy) }
+  end
+
+  context 'Validations' do
+    it 'must have a title' do
+      is_expected.to validate_presence_of :title
+    end
+
+    it 'must have created by' do
+      is_expected.to validate_presence_of :created_by
+    end
+  end
 end
