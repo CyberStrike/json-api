a087528315745d7f1eb6a2b74585da22e5f4a60c
Chris Scott <cyberstrike@gmail.com>
2017-11-06 03:28:54 -0500

    added items to todo spec

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :todo do
     title { Faker::Lorem.word }
     created_by { Faker::Number.number(10) }
+
+    factory :todo_with_items do
+      transient do
+        item_count 5
+      end
+
+      after(:create) do |todo, evaluator|
+        create_list(:item, evaluator.item_count, todo: todo)
+      end
+    end
   end
 end
